97e89f5e200772565eac69cc571f7f7e3ffa965d
Joshua N Ruiz <joshuanruiz@gmail.com>
2020-06-09 23:21:24 -0400

    move files out into a config folder

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
+const config = require('./private/config');
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-// app.get('/projects', (req, res) => res.sendFile(__dirname + '/routes/projects.html'));
 
-const port = 8080;
-const hostname = '167.99.14.95';
-app.listen(port, () => console.log(`Server running at http://${hostname}:${port}/`));
+app.listen(config.port);
