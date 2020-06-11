fce07e59c06ea612605c2fb1bf8051167ee4f6bd
Joshua N Ruiz <joshuanruiz@gmail.com>
2020-06-11 19:47:45 -0400

    add baseline for first project

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/projects/spotify-tracker/build'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/projects/spotify-tracker', (req, res) => res.sendFile(__dirname + '/projects/spotify-tracker/build/static/index.html'));
 
 app.listen(config.port);
